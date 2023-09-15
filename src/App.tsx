2d94faa8a0a1d82e8c9b87075c9f8aa594213b35
alexandreoliv29 <alexandre.foj@gmail.com>
2023-09-14 22:31:09 -0300

    deleting imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './components/Header'
 import { Videos } from './components/Videos'
@@ -9,7 +6,6 @@ import { Footer } from './components/Footer'
 import ScrollProgressBar from './components/ScrollProgressBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div style={{ backgroundColor: '#022c40', width: '100vw' }}>
--- src/components/ScrollProgressBar.tsx
+++ src/components/ScrollProgressBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ScrollProgressBar = () => {
     const [scrollPercentage, setScrollPercentage] = useState(0);
